51367645b8c98b7f66c7b0fc4977332acbbcee41
Grace Li <graceli@10-0-0-157.springload.co.nz>
2018-09-22 15:10:35 +1200

    Read unzipped csv file headers and compare headers with doc/Portfolio_Example.csv

--- features/report_download.feature
+++ features/report_download.feature
@@ -2,6 +2,9 @@ Feature: Open the data extract page and download portfolio report
 
 Scenario: Download portfolio report from data extract page
   Given I navigate to "https://qa.psuk.fluxtest.app/admin/smets_portfolio_reports/new"
-  And I login as admin
+  When I login as admin
   And I generate the report
   Then I can download
+  When I extract the zip file name "/PS_*.zip"
+  And I read the csv headers
+  Then I verify the headers are same as file "Portfolio_Example.csv"
--- features/step_definitions/report_download.rb
+++ features/step_definitions/report_download.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 Given("I login as admin") do
   steps %{
     And I enter "dev@powershop.co.nz" into "admin_email"
@@ -11,7 +13,7 @@ end
 When("I generate the report") do
   steps %{
     And I click on name "commit"}
-    sleep 2
+    sleep 5
     @driver.navigate.refresh
     steps %{
     And I wait until "File generated successfully." display
@@ -23,8 +25,10 @@ Then("I can download") do
     And I click on link "Download"
   }
   sleep 3
-  file = @download_path + "/PS_*.zip"
-  puts Dir[file.to_s]
+end
+
+When("I extract the zip file name {string}") do |file_name|
+  file = @download_path + file_name
   file = Dir[file.to_s][0]
   puts file
   require 'zip'
@@ -33,6 +37,25 @@ Then("I can download") do
      f_path=File.join(@download_path, f.name)
      FileUtils.mkdir_p(File.dirname(f_path))
      zip_file.extract(f, f_path) unless File.exist?(f_path)
+     FileUtils.remove_dir zip_file.to_s
    }
   }
+end
+
+And("I read the csv headers") do
+  csv_name = @download_path + "/PS_*.csv"
+  csv_file = Dir[csv_name.to_s][0]
+  puts csv_file.to_s
+  puts CSV.foreach(csv_file.to_s).first
+  @csv_headers = CSV.foreach(csv_file.to_s).first
+  FileUtils.remove_dir csv_file.to_s
+end
+
+Then("I verify the headers are same as file {string}") do |doc_name|
+  csv_name = File.join(File.dirname(__FILE__), '../doc/', doc_name)
+  puts csv_name
+  doc_headers = CSV.foreach(csv_name).first
+  puts doc_headers
+  # assert_equal(doc_headers, @csv_headers, failure_message = "Sorry! I feel not very correct :(")
+  puts "\nHeaders match? :  #{doc_headers.to_s == @csv_headers.to_s}"
 end
